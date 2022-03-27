7e03f088479a9aafa47973a26077247749bd1ba8
Venus-Kavunza <venusmwende@gmail.com>
2022-03-27 15:37:18 +0300

    It will add update image function

--- pics/models.py
+++ pics/models.py
@@ -2,20 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Pic(models.Model):
-    image = models.ImageField(upload_to='images/')
-    description = models.TextField()
-    name = models.CharField(max_length=200)
-    upload_date = models.DateTimeField(auto_now_add=True)
-     
-    
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.delete()
-
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
@@ -53,3 +39,43 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
+
+
+class Pic(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    name = models.CharField(max_length=200)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id ,image, description , name,category,location):
+        cls.objects.filter(id = id).update(image=image,description=description,name=name,category=category,location=location)
+
+        
+    # @classmethod
+    # def update_image(cls, id ,image):
+    #     cls.objects.filter(id = id).update(image=image)
+        
+    @classmethod
+    def get_image_by_id(cls,id):
+        image =cls.objects.filter(id= id).first()
+        return image
+    
+    @classmethod
+    def search_image(cls, search_category):
+        images = cls.objects.filter(category__name__icontains=search_category)
+        return images
+    
+    @classmethod
+    def filter_by_location(cls,search_location):
+        location = cls.objects.filter(location__name=search_location).all()
+        return location
