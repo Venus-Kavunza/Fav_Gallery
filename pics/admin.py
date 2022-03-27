c6364bc2db73b9f899bc0a348993839ccefa602e
Venus-Kavunza <venusmwende@gmail.com>
2022-03-27 16:29:33 +0300

    It will add location and image objects

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Pic, Category, Location 
 
 # Register your models here.
+admin.site.register(Pic)
+admin.site.register(Category)
+admin.site.register(Location)
--- pics/views.py
+++ pics/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from .models import Pic, Location, Category
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'home.html')
+    image=Pic.objects.all()
+    locations = Location.all_Locations()
+    return render(request, 'home.html', {'images': Pic, 'locations': Location})
