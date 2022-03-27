79c124cfd6e3b3f58c022a5a8711d7dfbd473d1d
Venus-Kavunza <venusmwende@gmail.com>
2022-03-27 15:33:27 +0300

    It will add the location function

--- pics/models.py
+++ pics/models.py
@@ -15,3 +15,25 @@ class Pic(models.Model):
 
     def delete_image(self):
         self.delete()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    
+    @classmethod
+    def all_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    @classmethod   
+    def update_location(cls, id, name):
+        cls.objects.filter(id=id).update(name=name)
+
+    def __str__(self):
+        return self.name
