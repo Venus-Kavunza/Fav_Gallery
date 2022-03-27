427eb2913f4f4b4ad10d299d52b20d557db079a5
Venus-Kavunza <venusmwende@gmail.com>
2022-03-27 15:31:21 +0300

    It will add the pic function

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Pic(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    name = models.CharField(max_length=200)
+    upload_date = models.DateTimeField(auto_now_add=True)
+     
