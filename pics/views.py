d05407ec17786f3255b28a2b0205eb60b2dc9dd1
Venus-Kavunza <venusmwende@gmail.com>
2022-03-27 16:41:54 +0300

    It will add error message on category search form

--- pics/views.py
+++ pics/views.py
@@ -7,3 +7,18 @@ def welcome(request):
     image=Pic.objects.all()
     locations = Location.all_Locations()
     return render(request, 'home.html', {'images': Pic, 'locations': Location})
+
+def get_category(request):
+    title = "Category"
+    locations = Location.objects.all()
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_category=request.GET.get("category")
+        searched_category=Pic.search_image(search_category)
+        message = f"{search_category}"
+
+
+        return render(request,'all-gallery/search.html',{'message':message,'images':searched_category,'title':title,'locations':locations})
+    else:
+        message = "Please enter any category"
+        return render(request, 'search.html',{"message":message})
