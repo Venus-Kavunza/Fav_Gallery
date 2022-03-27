1f8615a45570e5d0d6b94cd7691f22c64673693e
Venus-Kavunza <venusmwende@gmail.com>
2022-03-27 13:57:05 +0300

    It will add bootstrap3 to the base.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pics',
     'bootstrap3',
-    'pics.apps.PicsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
